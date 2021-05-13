d3aea4454bbb7871ef60c8af736b53a72246f018
Leonardo Lima <leo.sopranzetti@hotmail.com>
2021-05-13 14:47:15 -0300

    deploy

--- src/http/index.js
+++ src/http/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import provedor from '@/provedor'
 
 const http = axios.create({
-    baseURL: 'http://localhost:8080/',
+    baseURL: 'https://alltheanime.herokuapp.com/',
     headers: {
         'Accept': 'application/json', 
         'Content': 'application/json'
