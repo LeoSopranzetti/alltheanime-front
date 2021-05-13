7dfb0ad79925143472048d628c932ea2ad0a7aeb
Leonardo Lima <leo.sopranzetti@hotmail.com>
2021-05-13 14:42:52 -0300

    footer

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,17 @@
   <div id="app" class="container">
     
     <router-view/>
+    <Footer/>
   </div>
   </div>
 </template>
 
 <script>
 import BarraNavegacao from '@/components/BarraNavegacao'
+import Footer from '@/components/Footer'
     export default {
         components: {
-          BarraNavegacao
+          BarraNavegacao, Footer
         }
     }
 </script>
@@ -21,4 +23,5 @@ import BarraNavegacao from '@/components/BarraNavegacao'
 background: rgb(137,241,133);
 background: linear-gradient(176deg, rgba(137,241,133,1) 11%, rgba(127,255,184,1) 30%, rgba(11,68,41,1) 91%);
 }
+
 </style>
